e5bfc3f7067c37e3d5d5cc196dd8a3d1e9ad22e7
aholg <holgersson31@hotmail.com>
2015-11-20 11:19:17 +0100

    Update ConnectionHandler.java

--- ConnectionHandler.java
+++ ConnectionHandler.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package id2212.hangman;
 
 import java.io.BufferedInputStream;
@@ -15,8 +19,8 @@ import java.util.logging.Logger;
 /**
  *
  * @author Anton
- * 
- * Connection handler for the server. Generates a word for a hangman game and 
+ *
+ * Connection handler for the server. Generates a word for a hangman game and
  * checks input from client if it matches any of the characters of the word.
  */
 public class ConnectionHandler implements Runnable {
@@ -27,11 +31,13 @@ public class ConnectionHandler implements Runnable {
     private Socket clientSocket;
     private int attempts;
     private boolean gameover;
-/**
- * Initiates variables and retrieves a random word from a dictionary.
- * @param clientSocket Socket to receive and send messages to.
- */
-    public ConnectionHandler(Socket clientSocket) {
+
+    /**
+     * Initiates variables and retrieves a random word from a dictionary.
+     *
+     * @param clientSocket Socket to receive and send messages to.
+     */
+    public ConnectionHandler(Socket clientSocket) throws IOException {
         this.clientSocket = clientSocket;
         file = new File("C:/Users/Anton/Documents/KTH/Applikationer för internet/Hangman/words.txt");
         attempts = 10;
@@ -43,6 +49,7 @@ public class ConnectionHandler implements Runnable {
             for (int i = 0; i < gameWord.length; i++) {
                 gameWord[i] = "-";
             }
+            toClient(createMessage(false));
             //word = "yes";
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
@@ -51,14 +58,14 @@ public class ConnectionHandler implements Runnable {
 
     /**
      * Retrieves message from client and checks if it matches the game word.
-     * Runs until number of attempts have reached 0 or until correct word was guessed.
-     * Sends return message with the current state of the hangman game to client. 
-     * Example: Game word="electrode" 
-     * Client input: "e"
-     * Server answer: "e-e-----e"
+     * Runs until number of attempts have reached 0 or until correct word was
+     * guessed. Sends return message with the current state of the hangman game
+     * to client. Example: Game word="electrode" Client input: "e" Server
+     * answer: "e-e-----e"
      */
     @Override
     public void run() {
+
         try (BufferedInputStream in = new BufferedInputStream(clientSocket.getInputStream());) {
 
             byte[] msg = new byte[4096];
@@ -66,7 +73,7 @@ public class ConnectionHandler implements Runnable {
             while (gameover == false) {
                 int bytesRead = 0;
                 int n;
-
+                msg = new byte[4096];
                 while ((n = in.read(msg, bytesRead, 256)) != -1) {
                     bytesRead += n;
                     if (bytesRead == 4096) {
@@ -75,21 +82,17 @@ public class ConnectionHandler implements Runnable {
                     if (in.available() == 0) {
                         break;
                     }
-
                 }
                 String query = new String(msg).trim();
                 if (query.length() > 1) {
                     if (query.equals(word)) {
                         gameover = true;
-                        toClient("Correct! The word was '" + query + "' \n Your score: " + attempts);
-                        System.exit(1);
-                    } else {
+                        
+                        toClient(createMessage(true));
 
+                    } else {
                         attempts--;
-                        if (attempts == 0) {
-                            gameover = true;
-                            toClient("Game over, you lose.");
-                        }
+                        toClient(createMessage(false));
                     }
                 } else {
                     int i = word.indexOf(query);
@@ -98,53 +101,67 @@ public class ConnectionHandler implements Runnable {
                             gameWord[i] = query;
                             i = word.indexOf(query, i + 1);
                         }
-
+                        toClient(createMessage(false));
                     } else {
                         attempts--;
-                        if (attempts == 0) {
-                            gameover = true;
-                            toClient("Game over, you lose.");
-                        }
+                        toClient(createMessage(false));
                     }
-
                 }
-                StringBuilder builder = new StringBuilder();
-                for (String s : gameWord) {
-                    builder.append(s);
-                }
-                if (builder.toString().equals(word)) {
-                    gameover=true;
-                    toClient("Correct! The word was '" + word + "' \n Your score: " + attempts);
-
-                } else {
-                    builder.append(" Attempts left: ");
-                    builder.append(attempts);
-                    toClient(builder.toString());
-                }
-
+                
             }
 
         } catch (IOException ex) {
             Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
     }
-/**
- * Send a message to the client.
- * @param msg - Message to send.
- * @throws IOException 
- */
+
+    /**
+     * Creates a message to return to the client. Message depends on client
+     * guess.
+     *
+     * @return String to send to client.
+     */
+    private String createMessage(boolean win) {
+        StringBuilder builder = new StringBuilder();
+        String returnMsg;
+        for (String s : gameWord) {
+            builder.append(s);
+        }
+        if (builder.toString().equals(word)|| win==true) {
+            gameover = true;
+            returnMsg = "Correct! The word was '" + word + "' \n Your score: " + attempts;
+        } else if (attempts == 0) {
+            gameover = true;
+            returnMsg = "Game over, you lose.";
+        } else {
+            builder.append(" Attempts left: ");
+            builder.append(attempts);
+            returnMsg = builder.toString();
+
+        }
+        return returnMsg;
+    }
+
+    /**
+     * Send a message to the client.
+     *
+     * @param msg - Message to send.
+     * @throws IOException
+     */
     private void toClient(String msg) throws IOException {
         BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream());
         byte[] toClient = msg.getBytes();
         out.write(toClient, 0, toClient.length);
         out.flush();
     }
-/**
- * Chooses a random word from a file containing dictionary words.
- * @return - Random word
- * @throws FileNotFoundException 
- */
+
+    /**
+     * Chooses a random word from a file containing dictionary words.
+     *
+     * @return - Random word
+     * @throws FileNotFoundException
+     */
     private String randomWord() throws FileNotFoundException {
         String result = null;
         Random rand = new Random();
